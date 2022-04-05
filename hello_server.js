92f37439b479674bb3a3c4691b64ef8bd6162254
g-101 <gyreplicant@gmail.com>
2022-04-05 10:50:04 -0300

    criando servidor http

---
+++ hello_server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+// createServer() é responsável por levantar um servidor
+// o callback (req, res) => {} só é executado quando há uma requisição
+const server = http.createServer( (req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write("<h1>hello world</h1>");
+    res.end();
+});
+// o event-loop verifica constantemente se o servidor foi
+// requisitado e, quando recebe uma requisição, ele emite um evento
+// para que seja executado o callback.
+server.listen(3000, () => {
+    console.log("Servidor Hello World rodando! http://localhost:3000");
+});
+
+// Servidor está sendo executado na porta 3000 
+// e por padrão responde um resultado em html pela rota raiz "/"
