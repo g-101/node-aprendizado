0b23627a6daf8d00564cfb8232ae7255c7dadcc5
g-101 <gyreplicant@gmail.com>
2022-04-23 11:33:30 -0300

    html com modulo fs

---
+++ 2-NODE-CORE-MODULES/5-html-com-fs/index.js
@@ -0,0 +1,13 @@
+const http = require("http"); //importa o módulo http
+
+
+// cria o server que recebe um callback com os objetos request e response
+const server = http.createServer( (req, res) => {
+    res.statusCode = 200; // codigo status http
+    res.setHeader("Contenty-Type", "text/html"); //define um header para a resposta
+    //para finalizar o middleware:
+    res.end("<h1>Olá, meu primeiro servidor!</h1>"); // response retorna html 
+})
+
+// criar a escuta do servidor na porta 3000
+server.listen(3000, () => console.log(`Servidor online: http://localhost:3000`));
