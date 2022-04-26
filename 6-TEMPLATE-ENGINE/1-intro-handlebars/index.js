2e5d4b69ec0d134dc0936717e9fb230aa4068e65
g101 <gyreplicant@gmail.com>
2022-04-26 13:37:22 -0300

    setup e layouts com handlebars

---
+++ 6-TEMPLATE-ENGINE/1-intro-handlebars/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// O que é TEMPLATE ENGINE?
+
+
+//mapeia o template engine
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+app.get("/", (req, res) => {
+    res.render("home", { layout:false } ) // iniciando com falso pois n temos layout
+})
+
+app.listen(3000, () => console.log("Servidor online: http://localhost:3000") )
