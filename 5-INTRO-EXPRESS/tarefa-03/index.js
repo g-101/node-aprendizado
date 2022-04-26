44f817a6a4c376b9e8e346f02c9c92cd9bfaef00
g101 <gyreplicant@gmail.com>
2022-04-26 07:56:21 -0300

    comentando a tarefa

--- 5-INTRO-EXPRESS/tarefa-03/index.js
+++ 5-INTRO-EXPRESS/tarefa-03/index.js
@@ -1,18 +1,25 @@
-const express = require("express")
-const app = express()
-const path = require("path")
+// 1 = MODULOS
+const express = require("express") // importa o modulo express
+const app = express() // instancia o modulo express e armazena em app
+const path = require("path") // importa o modulo path
 
+// meus modulos de rotas
 const cats = require("./cats")
 const dogs = require("./dogs")
 
+// 2 - MIDDLEWARES
+// mapeia os static files
 app.use(express.static("public"))
-const basePath = path.join(__dirname, "templates")
-
+// mapeia os modulos de rotas importados
 app.use(cats)
 app.use(dogs)
-// home
+const basePath = path.join(__dirname, "templates")
+
+// 3-  ROTA
+// home router
 app.get("/", (req, res) => {
     res.sendFile(`${basePath}/index.html`) 
 })
 
+// configura porta do servidor de testes
 app.listen(5000, () => console.log("Servidor online: http://localhost:5000") )
