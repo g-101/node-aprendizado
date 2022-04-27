8996e377e817eb39fe6e9a14fb3cef38ab2ddbcf
g101 <gyreplicant@gmail.com>
2022-04-27 09:35:09 -0300

    repeticoes na view

---
+++ 6-TEMPLATE-ENGINE/5-repeticoes/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+//mapeia o template engine
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+app.get("/", (req, res) => {
+    const user = {
+        name: "Angela",
+        surname: "Silva",
+        age: 30,
+    }
+    const teste = "teste hahahh"
+    const auth = true
+    // vai renderizar as variaveis que eu colocar
+    res.render("home", { user:user, teste, auth })
+})
+
+app.get("/dashboard", (req, res) => {
+    const items = ["item A", "item B", "item C"]
+    res.render("dashboard", { items })
+})
+
+app.listen(3000, () => console.log("Servidor online: http://localhost:3000") )
